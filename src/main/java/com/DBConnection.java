17dc8364996010b920ebdec77bfd472e0787dbed
rajagary <rajagopal.arya@gmail.com>
2020-03-03 16:20:28 +0530

    test

--- src/main/java/com/DBConnection.java
+++ src/main/java/com/DBConnection.java
@@ -116,7 +116,7 @@ static void generateReport(DBCollection collection,BasicDBObject whereQuery,Stri
 	    fields.put("eventDate",1);
 	    fields.put("eventDetails",1);
 	    fields.put("transactionId",1);
-		DBCursor cursor = collection.find(whereQuery,fields).sort(new BasicDBObject("created_at",-1));
+		DBCursor cursor = collection.find(whereQuery,fields).sort(new BasicDBObject("created_atm",-1));
 		TreeMap<Double, String> sortedeventDetails = new TreeMap<Double, String>();
 	    while (cursor.hasNext()) {
 	    	DBObject tobj = cursor.next();
